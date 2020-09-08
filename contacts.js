60638dd553034367afecbf21e6f4fea9929a5caa
Maxim Kozin <maxa.kozin@gmail.com>
2020-09-08 16:35:24 +0300

    added express functionality

--- contacts.js
+++ contacts.js
@@ -1,54 +1,68 @@
 const fs = require("fs").promises;
 const path = require("path");
+const shortid = require("shortid");
 
 contactsPath = path.resolve(__dirname, "db", "contacts.json");
 
 async function listContacts() {
-  fs.readFile(contactsPath, "utf-8")
-    .then((contacts) => console.table(JSON.parse(contacts)))
-    .catch((err) => console.log(err.message));
+  return (contacts = await fs.readFile(contactsPath, "utf-8"));
 }
 
 async function getContactById(contactId) {
   const contacts = await fs.readFile(contactsPath, "utf-8");
-  const foundContact = JSON.parse(contacts).find(({ id }) => id === contactId);
+  const response = [];
+  const foundContact = JSON.parse(contacts).find(({ id }) => id == contactId);
   if (foundContact) {
-    console.table(foundContact);
-    return;
+    response.push(foundContact);
+    return response;
   }
-  console.log(`Contact with id: ${contactId} is not found`);
 }
 
 async function removeContact(contactId) {
   const contacts = await fs.readFile(contactsPath, "utf-8");
   const filteredContacts = JSON.parse(contacts).filter(
-    ({ id }) => id !== contactId
+    ({ id }) => id != contactId
   );
-  const contactExist = JSON.parse(contacts).some(({ id }) => id === contactId);
+  const contactExist = JSON.parse(contacts).some(({ id }) => id == contactId);
   if (!contactExist) {
     console.log(`Contact with id: ${contactId} was not found`);
     return;
   }
-  fs.writeFile(contactsPath, JSON.stringify(filteredContacts)).then(() => {
-    console.log(`Contact with id: ${contactId} was successfully removed`);
-    console.table(filteredContacts);
-  });
+  await fs.writeFile(contactsPath, JSON.stringify(filteredContacts));
+  return true;
 }
 
 async function addContact(name, email, phone) {
   const contacts = await fs.readFile(contactsPath, "utf-8");
   const contactsList = JSON.parse(contacts);
-  const id = contactsList.length > 0 ? [...contactsList].pop().id + 1 : 1;
   const contact = {
-    id,
+    id: shortid.generate(),
     name,
     email,
     phone,
   };
   contactsList.push(contact);
-  fs.writeFile(contactsPath, JSON.stringify(contactsList)).then(() =>
-    console.table(contactsList)
-  );
+  await fs.writeFile(contactsPath, JSON.stringify(contactsList));
+  return contact;
+}
+
+async function updateContact(id, name, email, phone) {
+  const response = await getContactById(id);
+  if (!response) {
+    return;
+  }
+
+  if (name) response[0].name = name;
+  if (email) response[0].email = email;
+  if (phone) response[0].phone = phone;
+
+  const contacts = await fs.readFile(contactsPath, "utf-8");
+  const parsedContacts = JSON.parse(contacts);
+  const filteredContacts = parsedContacts.filter((contact) => contact.id != id);
+  filteredContacts.push(response[0]);
+
+  await fs.writeFile(contactsPath, JSON.stringify(filteredContacts));
+  return response[0];
 }
 
 module.exports = {
@@ -56,4 +70,5 @@ module.exports = {
   getContactById,
   removeContact,
   addContact,
+  updateContact,
 };
--- index.js
+++ index.js
@@ -1,32 +1,143 @@
-const argv = require("yargs").argv;
+// const argv = require("yargs").argv;
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 const {
   listContacts,
   getContactById,
   removeContact,
   addContact,
+  updateContact,
 } = require("./contacts");
 
-function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      listContacts();
-      break;
+const app = express();
+const PORT = 3000;
+const path = "/contacts";
 
-    case "get":
-      getContactById(id);
-      break;
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(cors());
 
-    case "add":
-      addContact(name, email, phone);
-      break;
+app.get(`${path}`, async (req, res) => {
+  const response = await listContacts().then((data) => JSON.parse(data));
+  res.set("Content-Type", "application/json");
+  res.statusCode = 200;
+  res.json(response);
+});
 
-    case "remove":
-      removeContact(id);
-      break;
-
-    default:
-      console.warn("\x1B[31m Unknown action type!");
+app.get(`${path}/:contactId`, async (req, res) => {
+  const { contactId } = req.params;
+  const err = {
+    message: "Not found",
+  };
+  const response = await getContactById(contactId);
+  if (response) {
+    res.statusCode = 200;
+    res.send(response);
+    return;
   }
-}
+  res.statusCode = 404;
+  res.send(err);
+});
 
-invokeAction(argv);
+app.post(`${path}`, async (req, res) => {
+  const body = req.body[0];
+  const { name, email, phone } = body;
+  console.log(name);
+
+  if (name && email && phone) {
+    addContact(name, email, phone)
+      .then((data) => {
+        res.statusCode = 201;
+        res.send(data);
+      })
+      .catch((err) => console.log(err.message));
+    return;
+  }
+  if (!name || !email || !phone) {
+    let response = "";
+    if (!name) response = "name";
+    if (!email) response = "email";
+    if (!phone) response = "phone";
+    const err = {
+      message: `missing required ${response} field`,
+    };
+    res.statusCode = 404;
+    res.send(err);
+  }
+});
+
+app.delete(`${path}/:contactId`, async (req, res) => {
+  const { contactId } = req.params;
+  const err = {
+    message: "Not found",
+  };
+  const success = {
+    message: "contact deleted",
+  };
+  const response = await removeContact(contactId);
+  if (response) {
+    res.statusCode = 200;
+    res.send(success);
+    return;
+  }
+  res.statusCode = 404;
+  res.send(err);
+});
+
+app.patch(`${path}/:contactId`, async (req, res) => {
+  const { contactId } = req.params;
+  const { name, email, phone } = req.body;
+  console.log("name: ", name, "email: ", email, "phone: ", phone);
+  const missing = {
+    message: "missing fields",
+  };
+  const err = {
+    message: "Not found",
+  };
+
+  if (!name && !email && !phone) {
+    res.statusCode = 400;
+    res.send(missing);
+    return;
+  }
+
+  const response = await updateContact(contactId, name, email, phone);
+  if (!response) {
+    res.statusCode = 404;
+    res.send(err);
+    return;
+  }
+
+  res.statusCode = 200;
+  res.send(response);
+});
+
+app.listen(PORT, () => {
+  console.log("Server is working on port: ", PORT);
+});
+
+// function invokeAction({ action, id, name, email, phone }) {
+//   switch (action) {
+//     case "list":
+//       listContacts();
+//       break;
+
+//     case "get":
+//       getContactById(id);
+//       break;
+
+//     case "add":
+//       addContact(name, email, phone);
+//       break;
+
+//     case "remove":
+//       removeContact(id);
+//       break;
+
+//     default:
+//       console.warn("\x1B[31m Unknown action type!");
+//   }
+// }
+
+// invokeAction(argv);
