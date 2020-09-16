91503f3dec5cce8e407544c72de3b180ebcf282a
Maxim Kozin <maxa.kozin@gmail.com>
2020-09-16 15:46:08 +0300

    adding mongoose logic

---
+++ contacts/contacts.model.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      default: "Incognito",
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    subscription: {
+      type: String,
+      required: true,
+      default: "free",
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { versionKey: false }
+);
+
+class Contacts {
+  constructor() {
+    this.db = mongoose.model("Contacts", contactSchema);
+  }
+
+  getContacts = async () => {
+    return await this.db.find();
+  };
+
+  addContact = async (contact) => {
+    return await this.db.create(contact);
+  };
+
+  getContactById = async (contactId) => {
+    return this.db.findById(contactId);
+  };
+
+  deleteContact = async (contactId) => {
+    return this.db.findByIdAndRemove(contactId);
+  };
+
+  updateContact = async (contactId, newData) => {
+    return this.db.findByIdAndUpdate(contactId, newData, { new: true });
+  };
+}
+
+module.exports = new Contacts();
--- index.js
+++ index.js
@@ -1,143 +1,46 @@
-// const argv = require("yargs").argv;
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-} = require("./contacts");
+const mongoose = require("mongoose");
+const router = require("./contacts/router");
 
-const app = express();
-const PORT = 3000;
-const path = "/contacts";
+const DB_URI =
+  process.env.DB_URI ||
+  "mongodb+srv://admin:qwerty123@cluster0.ppmz7.mongodb.net/contacts-db?retryWrites=true";
 
-app.use(express.json());
-app.use(morgan("tiny"));
-app.use(cors());
+mongoose.set("useCreateIndex", true);
 
-app.get(`${path}`, async (req, res) => {
-  const response = await listContacts().then((data) => JSON.parse(data));
-  res.set("Content-Type", "application/json");
-  res.statusCode = 200;
-  res.json(response);
-});
+const serverInit = async () => {
+  await mongoose
+    .connect(DB_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    })
+    .then(() => console.log("Database connection successful"))
+    .catch((err) => {
+      console.log(err.message);
+      process.exit(1);
+    });
 
-app.get(`${path}/:contactId`, async (req, res) => {
-  const { contactId } = req.params;
-  const err = {
-    message: "Not found",
-  };
-  const response = await getContactById(contactId);
-  if (response) {
-    res.statusCode = 200;
-    res.send(response);
-    return;
-  }
-  res.statusCode = 404;
-  res.send(err);
-});
+  const app = express();
+  const PORT = process.env.PORT || 3000;
+  const path = "/contacts";
 
-app.post(`${path}`, async (req, res) => {
-  const body = req.body[0];
-  const { name, email, phone } = body;
-  console.log(name);
+  app.use(express.json());
+  app.use(morgan("tiny"));
+  app.use(cors());
 
-  if (name && email && phone) {
-    addContact(name, email, phone)
-      .then((data) => {
-        res.statusCode = 201;
-        res.send(data);
-      })
-      .catch((err) => console.log(err.message));
-    return;
-  }
-  if (!name || !email || !phone) {
-    let response = "";
-    if (!name) response = "name";
-    if (!email) response = "email";
-    if (!phone) response = "phone";
-    const err = {
-      message: `missing required ${response} field`,
-    };
-    res.statusCode = 404;
-    res.send(err);
-  }
-});
+  app.use(path, router);
 
-app.delete(`${path}/:contactId`, async (req, res) => {
-  const { contactId } = req.params;
-  const err = {
-    message: "Not found",
-  };
-  const success = {
-    message: "contact deleted",
-  };
-  const response = await removeContact(contactId);
-  if (response) {
-    res.statusCode = 200;
-    res.send(success);
-    return;
-  }
-  res.statusCode = 404;
-  res.send(err);
-});
+  app.use((err, req, res, next) => {
+    delete err.stack;
+    next(err);
+  });
 
-app.patch(`${path}/:contactId`, async (req, res) => {
-  const { contactId } = req.params;
-  const { name, email, phone } = req.body;
-  console.log("name: ", name, "email: ", email, "phone: ", phone);
-  const missing = {
-    message: "missing fields",
-  };
-  const err = {
-    message: "Not found",
-  };
+  app.listen(PORT, () => {
+    console.log("Server is working on port: ", PORT);
+  });
+};
 
-  if (!name && !email && !phone) {
-    res.statusCode = 400;
-    res.send(missing);
-    return;
-  }
-
-  const response = await updateContact(contactId, name, email, phone);
-  if (!response) {
-    res.statusCode = 404;
-    res.send(err);
-    return;
-  }
-
-  res.statusCode = 200;
-  res.send(response);
-});
-
-app.listen(PORT, () => {
-  console.log("Server is working on port: ", PORT);
-});
-
-// function invokeAction({ action, id, name, email, phone }) {
-//   switch (action) {
-//     case "list":
-//       listContacts();
-//       break;
-
-//     case "get":
-//       getContactById(id);
-//       break;
-
-//     case "add":
-//       addContact(name, email, phone);
-//       break;
-
-//     case "remove":
-//       removeContact(id);
-//       break;
-
-//     default:
-//       console.warn("\x1B[31m Unknown action type!");
-//   }
-// }
-
-// invokeAction(argv);
+serverInit();
